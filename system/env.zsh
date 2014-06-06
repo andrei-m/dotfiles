59aafc45fd02f9bb8baa5c459dd2fb429f3366d4
Andrei Mackenzie <andreimackenzie@gmail.com>
2014-06-06 15:46:35 -0400

    setup for rvm

--- system/env.zsh
+++ system/env.zsh
@@ -1,4 +1,3 @@
-export EDITOR='subl'
-export GEM_PATH='/var/lib/gems/1.8'
-export GEM_HOME='/var/lib/gems/1.8'
+export EDITOR='vim'
+source $HOME/.rvm/scripts/rvm
 
--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,5 @@
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export GOPATH="$HOME/dev/go"
 export GOROOT="/usr/local/go"
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/var/lib/gems/1.8/bin:$GOPATH/bin:$PATH"
+export PATH="/var/lib/gems/1.8/bin:./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$GOPATH/bin:$PATH"
 
