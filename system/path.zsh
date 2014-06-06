6b268fabe3e10e83c333c716364390dee57d89b7
Andrei Mackenzie <andreimackenzie@gmail.com>
2014-06-06 15:34:51 -0400

    configure path for go

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,5 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/var/lib/gems/1.8/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export GOPATH="$HOME/dev/go"
 export GOROOT="/usr/local/go"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/var/lib/gems/1.8/bin:$GOPATH/bin:$PATH"
 
