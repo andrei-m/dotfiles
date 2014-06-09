880beea92a4b6908235272677102f13102efd098
Andrei Mackenzie <andrei@clypd.com>
2014-06-09 16:17:31 -0400

    go cleanup; syntax highlighting for vim

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,3 @@
-export GOPATH="$HOME/dev/gillnet/go:$HOME/dev/go"
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/var/lib/gems/1.8/bin:$HOME/dev/gillnet/go/bin:$HOME/dev/go/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
