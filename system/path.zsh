f690f48ae264ee558d6581af8540f21a9bc64a23
Andrei Mackenzie <andrei@clypd.com>
2014-06-09 12:02:49 -0400

    go path setup

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/var/lib/gems/1.8/bin:$PATH"
-
+export GOPATH="$HOME/go"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/var/lib/gems/1.8/bin:$GOPATH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
