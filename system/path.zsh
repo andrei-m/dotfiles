ddc6e6107a0f2cfed1979fe08f72b46b2e9b9a70
Andrei Mackenzie <andrei@clypd.com>
2014-06-09 16:04:58 -0400

    update path for go

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export GOPATH="$HOME/go"
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/var/lib/gems/1.8/bin:$GOPATH/bin:$PATH"
+export GOPATH="$HOME/dev/gillnet/go:$HOME/dev/go"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/var/lib/gems/1.8/bin:$HOME/dev/gillnet/go/bin:$HOME/dev/go/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
