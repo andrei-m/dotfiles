a308b682712ca2d3756d5050b9f4cb57026fb61f
Andrei Mackenzie <andrei@clypd.com>
2020-07-22 10:01:06 -0400

    Include third_party Go binaries in PATH

--- go/path.zsh
+++ go/path.zsh
@@ -1,3 +1,3 @@
 export GOPATH="$HOME/dev/gillnet/go"
 export PATH="/usr/local/opt/go@1.12/bin:$PATH"
-export PATH="$GOPATH/bin:$HOME/dev/go/bin:$(go env GOROOT)/bin:$PATH"
+export PATH="$GOPATH/bin:$GOPATH/bin/third_party:$HOME/dev/go/bin:$(go env GOROOT)/bin:$PATH"
